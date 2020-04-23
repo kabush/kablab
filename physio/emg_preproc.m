2171ff22b1315651f585c2e24aa8d7022c7ad505
Keith Bush <kabush@gmail.com>
2020-04-23 13:46:03 -0500

    Updated physio to include filtering of EMG and new box plot

--- physio/emg_preproc.m
+++ physio/emg_preproc.m
@@ -18,61 +18,19 @@ function emg = emg_preproc(proj,TRs,data)
 end_of_run=TRs.*proj.param.mri.TR.*proj.param.physio.hz_emg; %%num TRs, s/TR, samples/s = total samples
 data = data(1:end_of_run);
 
-emg = data; 
+half_samp = proj.param.physio.hz_emg/2;
+high = proj.param.physio.emg.filt_high;
+low = proj.param.physio.emg.filt_low;
 
-%%% *** TICKET ***
-%%% This is for course-grained piloting. Need to tune 
-%%% filtering parameters.
+[B A] = butter(1,[high/half_samp low/half_samp]);
 
-% 
-% % ****************************************
-% % set-up time/freq conversions
-% % ****************************************
-% fsmp = proj.param.physio.hz_emg;  %sampling frequency
-% T = 1/fsmp;
-% N = numel(data);
-% f = fsmp*(0:(N/2))/N;
-% 
-% %compute freq domain
-% fy = fft(data);
-% 
-% % debug
-% % compute power spectrums
-% p2 = abs(fy/N); % 2-sided power spectrum
-% p1 = p2(1:N/2+1);
-% p1(2:end-1) = 2*p1(2:end-1);
-% 
-% % ****************************************
-% % plot power spectrum
-% % ****************************************
-% figure(1)
-% plot(f,p1);
-% title('Single-Sided Amplitude Spectrum of X(t)');
-% xlabel('f (Hz)');
-% ylabel('|p1(f)|');
-% 
-% % ****************************************
-% % filter
-% % ****************************************
-% 
-% %set-up the filter
-% flo = numel(find(f<proj.param.physio.emg.filt_low));
-% fhi = numel(find(f<proj.param.physio.emg.filt_high));
-% fltr_vec = zeros(size(fy));
-% fltr_vec(flo:fhi)=1;
-% 
-% %filter the freq space
-% fltr_fy = fy.*fltr_vec;
-% 
-% %invert back to time domain
-% iy = ifft(fy);
-% fltr_iy = ifft(fltr_fy);
-% 
-% % take real component for plotting      
-% emg = real(fltr_iy);
-% 
-% debug
-% figure(2)
-% plot(data,'b-');
-% hold on;
-% plot(emg,'r-');
+if(proj.param.physio.emg.filt_type==1)
+    %unidirection
+    fdata=filter(B,A,data);
+else
+    %bidirectional
+    fdata=filtfilt(B,A,data);
+end
+
+emg = zscore(fdata);
+
